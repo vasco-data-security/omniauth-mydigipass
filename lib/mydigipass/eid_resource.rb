10c5a9f707acdd18e649253320dcd9183106b71b
Vince Verberckt <vince.verberckt@vasco.com>
2015-06-10 14:34:42 +0200

    Namespace the resource in the correct module.

--- lib/mydigipass/eid_resource.rb
+++ lib/mydigipass/eid_resource.rb
@@ -1,9 +1,9 @@
-require 'resource'
+require 'mydigipass/resource'
 
-class EidResource < Resource
-
-  def fetch_data(etag_header)
-    access_token.get(resource_url, headers: {'ETag' => etag_header}).parsed
+module Mydigipass
+  class EidResource < Resource
+    def fetch_data(etag_header)
+      access_token.get(resource_url, headers: {'ETag' => etag_header}).parsed
+    end
   end
-
 end
